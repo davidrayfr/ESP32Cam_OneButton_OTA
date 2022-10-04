731365bcbe941fde1a7a9165ff186fbbf97587c7
davidrayfr <davidrayfr@gmail.com>
2022-10-04 16:46:13 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+
+const char* ssid = "Your_SSID";
+const char* password = "Your_Password";
+
+AsyncWebServer server(80);
+
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected! ");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hello! This is a simple text message sent from ESP32. Microcontrollerslab");
+  });
+
+  AsyncElegantOTA.begin(&server);    
+  server.begin();
+  Serial.println("HTTP server started!");
+}
+
+void loop(void) {
+  AsyncElegantOTA.loop();
+}  
